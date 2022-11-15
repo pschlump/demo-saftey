98b03aa85220237a1a9487992102bdf91e50bde1
Philip Schlump <pschlump@gmail.com>
2022-11-15 10:46:36 -0700

    Add Instructions

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-# safe
+# safe space demo
 
-## Project setup
+
+## Project Install and Setup
+
+This a `vue.js` project for the front end and a `go` back end with a `postresql`
+database.   The front end requires `node.js` and `npm` to work on the front end.
+The back end server is in the ./safe-server directory and requires Go and PostgreSQL
+to be installed.   See the README.md in ./safe-server.
+
+### Project setup
 ```
 npm install
 ```
--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,5 +1,117 @@
 <template>
-  <div class="sign-in">
-    <h1>This is an sign-in page</h1>
-  </div>
+	<div class="login">
+		<h1> Sign In </h1>
+        <form @submit.prevent="handleSubmit">
+			<div class="input-form">
+
+				<div class="form-group">
+					<div>Email</div>
+					<div class="center">
+						<input type="text" v-model="email" name="email" class="form-control" :class="{ 'is-invalid': submitted && !email }" autocorrect="off" autocapitalize="none" />
+					</div>
+					<div class="center">
+						<div v-show="submitted && !email" class="invalid-feedback">Email Address is required</div>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div>Password</div>
+					<div class="center">
+						<input type="password" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" />
+					</div>
+					<div class="center">
+						<div v-show="submitted && !password" class="invalid-feedback">Password is required</div>
+					</div>
+				</div>
+
+			</div>
+			<div class="center">
+				<button @click="loginPt1()" class="btn btn-primary btn-two btn-std">
+					Sign In Now 
+				</button>
+				<button @click="forgotten()" class="btn btn-primary btn-one btn-std">
+					Forgotten Password 
+				</button>
+			</div>
+		</form>
+	</div>
 </template>
+
+<script>
+/* eslint-disable no-console */
+
+import { mapState, mapActions } from 'vuex'
+// @ is an alias to /src
+import { router } from '../router/index.js';
+
+export default {
+	name: 'LoginView'
+    , data () {
+        return {
+            email: '',
+            password: '',
+            submitted: false,
+        }
+    }
+    , created () {
+        this.logout(); 
+    }
+    , computed: {
+        ...mapState('account', ['status'])
+    }
+	, methods: {
+        ...mapActions('account', ['login', 'logout'])
+		, loginPt1 () {
+		}
+		, forgotten () {
+			this.logout();
+			router.push( '/recover-password/recover-password' );
+		}
+		, handleSubmit () {
+			this.submitted = true;
+			const { email, password } = this;
+			if (email && password) {
+				this.login({ email, password, "xsrf_id": window.xsrf_id })	
+			}
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+.login {
+	height: 100vh;
+}
+.input-form input {
+	width: 90vw;
+	background-color: #ccc;
+	max-width: 450px;
+}
+.center {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.btn-next {
+	position: absolute;
+	bottom: 20px;
+	align-items: center;
+}
+.btn-std {
+	min-width: 200px;
+	font-weight: 700;
+	font-size: 18px;
+	background-color: white;
+}
+.btn-one {
+	position: absolute;
+	bottom: 20px;
+	align-items: center;
+}
+.btn-two {
+	position: absolute;
+	bottom: 80px;
+	align-items: center;
+}
+</style>
